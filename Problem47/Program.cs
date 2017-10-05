257899bfbfcaa9679e02ef5d442f0cbf99fb85a9
sidstip <sidstip@WS42342>
2017-10-05 17:03:31 +0200

    Solved Problem 48

--- Problem47/Program.cs
+++ Problem47/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,7 +22,75 @@ namespace Problem47
                 646 = 2 × 17 × 19.
                 Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
             */
+            bool bFound = false;
+            int consecutiveNumbersFound = 0;
+            int currentInteger = 629; //2*3*5*7 smallest possible number with 4 distinct factors
+            var primes = GetPrimes(1000000);
+            while (!bFound)
+            {
+                //Take next integer
+                currentInteger++;
 
+                int distinctFactors = DetermineDistinctFactors(primes, currentInteger);
+                if (distinctFactors != 4)
+                {
+                    consecutiveNumbersFound = 0;
+                    continue;
+                }
+
+                consecutiveNumbersFound++;
+                if (consecutiveNumbersFound == 4)
+                {
+                    bFound = true;
+                }
+            }
+            Console.WriteLine($"Last number of sequence found is {currentInteger}");
+
+        }
+
+        private static int DetermineDistinctFactors(BitArray primes, int currentInteger)
+        {
+            //SearchStart
+            int startingPoint = (int) Math.Floor(Math.Pow(currentInteger, 0.5));
+            var factors = new HashSet<int>();
+            int remainder = currentInteger;
+            for (int i = startingPoint; i > 1; i--)
+            {
+                if (!primes.Get(i)) continue;
+
+                if (remainder % i != 0) continue;
+                while (remainder % i == 0)
+                {
+                    remainder /= i;
+                }
+                factors.Add(i);
+            }
+            if (primes.Get(remainder))
+            {
+                factors.Add(remainder);
+            }
+
+            return factors.Count;
+
+        }
+
+        public static BitArray GetPrimes(int upperLimit)
+        {
+            var primes = new BitArray(upperLimit);
+            primes.SetAll(true);
+            primes.Set(0, false);
+            primes.Set(1, false);
+            for (int i = 0; i * i < upperLimit; i++)
+            {
+                if (primes.Get(i))
+                {
+                    for (int j = i * i; j < upperLimit; j += i)
+                    {
+                        primes.Set(j, false);
+                    }
+                }
+            }
+            return primes;
         }
     }
 }
---
+++ Problem48/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Problem28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * ¹²³⁴⁵⁶⁷⁸⁹⁰
+                The series, 1¹ + 2² + 3³ + ... + 10¹⁰ = 10405071317.
+                Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+            */
+            // n * 10 + n * 
+
+            BigInteger sum = 0;
+            sum += GetValue(1, 1000);
+            //sum += GetValue(10, 100);
+            //sum += GetValue(100, 1000);
+            //sum += 10001000;
+
+            string result = String.Concat(sum.ToString().ToCharArray().Reverse().Take(10).Reverse().ToList());
+
+            Console.WriteLine($"Last digits : {result}");
+            Console.ReadLine();
+        }
+
+        private static BigInteger GetValue(int start, int stop)
+        {
+            BigInteger sum = 0;
+            for (int i = start; i <= stop; i++)
+            {
+                sum += BigInteger.Pow(i, i);
+            }
+            return sum;
+        }
+    }
+}
